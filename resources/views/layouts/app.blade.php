6610292f1ef55f7ef2209197f59138ad51a66ff7
salmakhoirunisa <nisasalma335@gmail.com>
2022-02-25 13:30:11 +0700

    modul 6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,56 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Dashboard</title>
+
+    @include('partials/css');
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">>
+        <!-- HEADER MOBILE-->
+        @include('partials/header-mobile')
+        <!-- END HEADER MOBILE-->
+
+        <!-- MENU SIDEBAR-->
+        @include('partials/sidebar')
+        <!-- END MENU SIDEBAR-->
+
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            
+            <!-- HEADER DESKTOP-->
+            @include('partials/header')
+            <!-- HEADER DESKTOP-->
+
+            <!-- MAIN CONTENT-->
+            @yield('content')
+            <!-- END MAIN CONTENT-->
+            <!-- END PAGE CONTAINER-->
+        </div>
+
+    </div>
+
+@include('partials/script')
+ 
+</body>
+
+</html>
+<!-- end document-->
+           <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
